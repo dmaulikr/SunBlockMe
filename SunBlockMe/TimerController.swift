d7e2c14f4002285713953a18e4d942ec938752e2
Angel Baek <angelbaek1@gmail.com>
2015-07-09 16:31:41 -0400

    add constraints to start button

--- SunBlockMe/TimerController.swift
+++ SunBlockMe/TimerController.swift
@@ -9,20 +9,10 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    
-    @IBOutlet weak var countdownLabel: UILabel!
-    var count = 10
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
-    }
-    
-    func update() {
-        
-        if(count > 0)
-        {
-            countdownLabel.text = String(count--)
-        }
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
